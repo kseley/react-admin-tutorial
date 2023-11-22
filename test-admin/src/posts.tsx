396d7d8fdf61df9cc900e3f1be4707ab82f329d9
kseley <66807841+kseley@users.noreply.github.com>
2023-11-21 18:50:32 -0600

    completed tutorial need real dataprovider

---
+++ test-admin/src/posts.tsx
@@ -0,0 +1,44 @@
+import { useRecordContext } from 'react-admin';
+import { Datagrid, EditButton, List, ReferenceField, TextField, Edit, Create, SimpleForm, ReferenceInput, TextInput } from 'react-admin';
+
+const postFilters = [
+    <TextInput source="q" label="Search" alwaysOn />,
+    <ReferenceInput source="userId" label="User" reference="users" />
+];
+
+export const PostList = () => (
+    <List filters={postFilters}>
+        <Datagrid>
+            <TextField source="id" />
+            <ReferenceField source="userId" reference="users" link="show" />
+            <TextField source="title" />
+            <EditButton />
+        </Datagrid>
+    </List>
+);
+
+const PostTitle = () => {
+    const record = useRecordContext();
+    return <span>Post {record ? `"${record.title}"` : ''}</span>;
+};
+
+export const PostEdit = () => (
+    <Edit title={<PostTitle />}>
+        <SimpleForm>
+            <TextInput source="id" InputProps={{ disabled: true }} />
+            <ReferenceInput source="userId" reference="users" />
+            <TextInput source="title" />
+            <TextInput source="body" multiline rows={5} />
+        </SimpleForm>
+    </Edit>
+);
+
+export const PostCreate = () => (
+    <Create>
+        <SimpleForm>
+            <ReferenceInput source="userId" reference="users" />
+            <TextInput source="title" />
+            <TextInput source="body" multiline rows={5} />
+        </SimpleForm>
+    </Create>
+);
